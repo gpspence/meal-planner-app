2d8a339ae999f15ffcd2615627032c6e9275d4fe
gpspence <105173090+gpspence@users.noreply.github.com>
2025-06-08 21:48:18 +0100

    Configure nav tab link functionality with router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom';
-import { HomePage } from './pages/Home.page';
+import { HomePage } from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
+import NotFoundPage from './pages/NotFoundPage';
+import RecipesPage from './pages/RecipesPage';
+import AnalyticsPage from './pages/AnalyticsPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
+      <Route path='/recipes' element={<RecipesPage />}/>
+      <Route path='/analytics' element={<AnalyticsPage />}/>
+      <Route path='*' element={<NotFoundPage />}/>
     </Route>
   )
 );
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,13 +2,32 @@ import { Title, Container, Tabs } from '@mantine/core';
 import { FaCalendarDay } from 'react-icons/fa';
 import { GiCook } from 'react-icons/gi';
 import { GoGraph } from 'react-icons/go';
+import { useNavigate, useParams } from 'react-router-dom';
 import classes from './Navbar.module.css';
 
+interface TabRoutes {
+    Planner: string
+    Recipes: string
+    Analytics: string
+}
+
 const Navbar = () => {
+
+    const navigate = useNavigate();
+    const { tabValue } = useParams();
+    const tabRoutes: TabRoutes = {
+        Planner: '/',
+        Recipes: '/recipes',
+        Analytics: '/analytics'
+    }
+
     return (
         <Container size="responsive">
             <Title order={2}>Weekly Meal Planner</Title>
-            <Tabs defaultValue="Planner">
+            <Tabs
+                value={tabValue}
+                onChange={(value) => navigate(tabRoutes[value ? value as keyof TabRoutes : "Planner"])}
+            >
                 <Tabs.List>
                     <Tabs.Tab value="Planner" leftSection={<FaCalendarDay />}>
                         Planner
