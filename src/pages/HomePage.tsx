12a50f382cc97a416aa08317997ff7d7cadc2070
gpspence <105173090+gpspence@users.noreply.github.com>
2025-06-10 21:40:29 +0100

    Add sections to planner page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,29 @@
-import { Container, Flex } from '@mantine/core';
+import DayPlanner from '@/components/DayPlanner/DayPlanner';
+import { Container, Flex, Grid, Paper, Stack, Text } from '@mantine/core';
 
 export function HomePage() {
   return (
-    <Container>
-      <Flex>
-        
-      </Flex>
+    <Container size='responsive'>
+      <Grid>
+        <Grid.Col bg='blue' span={4}>
+          <Stack>
+          <Container bg='green' m={0}>
+              <DayPlanner />
+            </Container>
+            <Container bg='red' m={0} p={0}>
+              <Stack>
+                <Paper shadow='sm' p='xs'>
+                  <Text size='xs'>Test</Text>
+                </Paper>
+              </Stack>
+            </Container>
+          </Stack>
+        </Grid.Col>
+        <Grid.Col bg='green' span={8}>
+          2
+
+        </Grid.Col>
+      </Grid>
     </Container>
   );
 }
