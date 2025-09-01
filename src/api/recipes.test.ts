a25d5383d7276e21a38ec294bae9cf7ae5ae96b6
gpspence <105173090+gpspence@users.noreply.github.com>
2025-09-01 21:17:04 +0100

    Add tests for strings.ts

---
+++ src/api/recipes.test.ts
@@ -0,0 +1,7 @@
+import { createRecipe, deleteSingleRecipe, loadRecipes, updateRecipe } from './recipes';
+
+describe('test recipe api functions', () => {
+  it('generic test', () => {
+    expect(1).toBe(1);
+  });
+});
--- src/utils/strings.test.ts
+++ src/utils/strings.test.ts
@@ -1,11 +1,21 @@
 import { cleanTitle, toTitleCase } from './strings';
 
 describe('string utils', () => {
-  it.each([['test lowercase', 'Test Lowercase']])(
-    'converts a string to title case',
+  it.each([
+    ['test lowercase', 'Test Lowercase'],
+    ['TEST UPPERCASE', 'Test Uppercase'],
+    ['special* charact_ers', 'Special* Charact_ers'],
+  ])('converts a string to title case', (value: string, expected: string) => {
+    expect(toTitleCase(value)).toBe(expected);
+  });
+
+  it.each([
+    ['test_one', 'Test One'],
+    ['Test_two*', 'Test Two*'],
+  ])(
+    'converts string to title case and replace underscore with space',
     (value: string, expected: string) => {
-      const result = toTitleCase(value);
-      expect(result).toBe(expected);
+      expect(cleanTitle(value)).toBe(expected);
     }
   );
 });
